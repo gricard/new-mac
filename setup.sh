187ca5023d10d122c8160e87d177f19080b82153
gabe <gabe@gabes-MacBook-Pro.local>
2019-01-26 07:08:29 -0500

    Reduce key repeat delay More clean up

--- setup.sh
+++ setup.sh
@@ -240,15 +240,6 @@ defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 # Finder: show hard drives on desktop
 defaults write com.apple.finder ShowHardDrivesOnDesktop -int 1
 
-## Remove the auto-hiding Dock delay
-#defaults write com.apple.dock autohide-delay -float 0
-#
-## Automatically hide and show the Dock
-#defaults write com.apple.dock autohide -bool true
-#
-## Only Show Open Applications In The Dock  
-#defaults write com.apple.dock static-only -bool true
-
 # Shrink dock icons
 defaults write com.apple.dock tilesize -int 45
 
@@ -256,7 +247,7 @@ defaults write com.apple.dock tilesize -int 45
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
 # Disable the warning when changing a file extension
-#defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
 # Automatically quit printer app once the print jobs complete
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
@@ -275,24 +266,8 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 # Minimize windows into their application’s icon
 defaults write com.apple.dock minimize-to-application -bool true
 
-# Automatically hide and show the Dock
-#defaults write com.apple.dock autohide -bool true
-
 # Don’t show recent applications in Dock
 defaults write com.apple.dock show-recents -bool false
-
-## Menu bar: hide the Time Machine, User icons, but show the volume Icon.
-#for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
-#	defaults write "${domain}" dontAutoLoad -array \
-#		"/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
-#		"/System/Library/CoreServices/Menu Extras/User.menu"
-#done
-#defaults write com.apple.systemuiserver menuExtras -array \
-#	"/System/Library/CoreServices/Menu Extras/Volume.menu" \
-#	"/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
-#	"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
-#	"/System/Library/CoreServices/Menu Extras/Battery.menu" \
-#	"/System/Library/CoreServices/Menu Extras/Clock.menu"
 
 # Show battery percentage in menu
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
@@ -308,10 +283,6 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 # Disable auto-correct
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# Use function F1, F, etc keys as standard function keys
-#defaults write NSGlobalDomain com.apple.keyboard.fnState -bool true
-
-
 ###############################################################################
 # Screenshots / Screen                                                        #
 ###############################################################################
@@ -319,26 +290,6 @@ defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 # Require password immediately after sleep or screen saver begins"
 defaults write com.apple.screensaver askForPassword -int 1
 defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-###############################################################################
-# Address Book, Dashboard, iCal, TextEdit, and Disk Utility                   #
-###############################################################################
-
-# Use plain text mode for new TextEdit documents
-#defaults write com.apple.TextEdit RichText -int 0
-
-###############################################################################
-# Spotlight                                                                   #
-###############################################################################
-
-# Hide Spotlight tray-icon (and subsequent helper)
-#sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
-# Disable Spotlight indexing for any volume that gets mounted and has not yet
-# been indexed before.
-# Use `sudo mdutil -i off "/Volumes/foo"` to stop indexing any volume.
-#sudo defaults write /.Spotlight-V100/VolumeConfiguration Exclusions -array "/Volumes"
-# Load new settings before rebuilding the index
-#killall mds
 
 ###############################################################################
 # Trackpad, mouse, keyboard, Bluetooth accessories, and input                 #
@@ -351,16 +302,6 @@ defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 # Stop iTunes from responding to the keyboard media keys
 launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
 
-# Disable force click and haptic feedback
-#defaults write ~/Library/Preferences/com.apple.AppleMultitouchTrackpad.plist ForceSuppressed -bool true
-
-## Mouse settings
-#defaults write com.apple.driver.AppleBluetoothMultitouch.mouse.plist MouseOneFingerDoubleTapGesture -int 0
-#defaults write com.apple.driver.AppleBluetoothMultitouch.mouse.plist MouseTwoFingerDoubleTapGesture -int 3
-#defaults write com.apple.driver.AppleBluetoothMultitouch.mouse.plist MouseTwoFingerHorizSwipeGesture -int 2
-#defaults write ~/Library/Preferences/.GlobalPreferences.plist com.apple.mouse.scaling -float 3
-#defaults write ~/Library/Preferences/.GlobalPreferences.plist com.apple.swipescrolldirection -boolean NO
-
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
@@ -369,7 +310,7 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain KeyRepeat -int 2
 
 # Reduce delay to repeat
-defaults write NSGlobalDomain InitialKeyRepeat -int 35
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Increase trackpad speed
 defaults write NSGlobalDomain com.apple.trackpad.scaling -int 2
