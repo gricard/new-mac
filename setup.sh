902f803346cbe0b6668d89a89f39b5e6ea1a1e0c
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2019-01-26 07:30:13 -0500

    Added git/github setup

--- setup.sh
+++ setup.sh
@@ -91,6 +91,76 @@ brew update
 brew tap caskroom/cask
 
 
+#############################################
+### Generate ssh keys & add to ssh-agent
+### See: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+#############################################
+
+echo "Generating ssh keys, adding to ssh-agent..."
+read -p 'Input email for ssh key: ' useremail
+
+echo "Use default ssh file location, enter a passphrase: "
+ssh-keygen -t rsa -b 4096 -C "$useremail"  # will prompt for password
+eval "$(ssh-agent -s)"
+
+# Now that sshconfig is synced add key to ssh-agent and
+# store passphrase in keychain
+ssh-add -K ~/.ssh/id_rsa
+
+# If you're using macOS Sierra 10.12.2 or later, you will need to modify your ~/.ssh/config file to automatically load keys into the ssh-agent and store passphrases in your keychain.
+
+if [ -e ~/.ssh/config ]
+then
+    echo "ssh config already exists. Skipping adding osx specific settings... "
+else
+	echo "Writing osx specific settings to ssh config... "
+   cat <<EOT >> ~/.ssh/config
+	Host *
+		AddKeysToAgent yes
+		UseKeychain yes
+		IdentityFile ~/.ssh/id_rsa
+EOT
+fi
+
+#############################################
+### Add ssh-key to GitHub via api
+#############################################
+
+echo "Adding ssh-key to GitHub (via api)..."
+echo "Important! For this step, use a github personal token with the admin:public_key permission."
+echo "If you don't have one, create it here: https://github.com/settings/tokens/new"
+
+retries=3
+SSH_KEY=`cat ~/.ssh/id_rsa.pub`
+
+for ((i=0; i<retries; i++)); do
+      read -p 'GitHub username: ' ghusername
+      read -p 'Machine name: ' ghtitle
+      read -sp 'GitHub personal token: ' ghtoken
+
+      gh_status_code=$(curl -o /dev/null -s -w "%{http_code}\n" -u "$ghusername:$ghtoken" -d '{"title":"'$ghtitle'","key":"'"$SSH_KEY"'"}' 'https://api.github.com/user/keys')
+
+      if (( $gh_status_code -eq == 201))
+      then
+          echo "GitHub ssh key added successfully!"
+          break
+      else
+			echo "Something went wrong. Enter your credentials and try again..."
+     		echo -n "Status code returned: "
+     		echo $gh_status_code
+      fi
+done
+
+[[ $retries -eq i ]] && echo "Adding ssh-key to GitHub failed! Try again later."
+
+#############################################
+### Setup git user info
+#############################################
+read -p 'Git email address: ' gitEmail
+read -p 'Git name: ' gitName
+git config --global user.email "$gitEmail"
+git config --global user.name "$gitName"
+
 ##############################
 # Install via Brew           #
 ##############################
